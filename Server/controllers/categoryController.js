698fbad4fd240067794718ceb11b75a09a357482
pratiushp <pratiushprasain@gmail.com>
2023-07-02 01:03:15 +0545

    CRUD of Category

---
+++ Server/controllers/categoryController.js
@@ -0,0 +1,125 @@
+import categoryModel from "../models/categoryModel.js";
+import slugify from "slugify";
+
+export const createCategoryController = async (req, res) => {
+  try {
+    const { name } = req.body;
+    if (!name) {
+      return res.status(401).send({
+        message: "Name is Required",
+      });
+    }
+
+    const existingCategory = await categoryModel.findOne({ name });
+    if (existingCategory) {
+      return res.status(200).send({
+        success: true,
+        message: "Category already exist",
+      });
+    }
+    const category = await new categoryModel({
+      name,
+      slug: slugify(name),
+    }).save();
+    res.status(201).send({
+      success: true,
+      message: "New Category Created",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error in Category",
+    });
+  }
+};
+
+//Update Category Controller
+
+export const updateCategoryController = async (req, res) => {
+  try {
+    const { name } = req.body;
+    const { id } = req.params;
+    const category = await categoryModel.findByIdAndUpdate(
+      id,
+      { name, slug: slugify(name) },
+      { new: true }
+    );
+    res.status(200).send({
+      success: true,
+      message: "updated Successfully",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error in Updating ",
+    });
+  }
+};
+
+//get all category
+export const categoryController = async (req, res) => {
+  try {
+    const category = await categoryModel.find({});
+    res.status(200).send({
+      success: true,
+      message: "All Category are listed",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error in Fetching the Category ",
+    });
+  }
+};
+
+//Single Category
+export const singleCategoryController = async (req, res) => {
+  try {
+    const category = await categoryModel.findOne({ slug: req.params.slug });
+    res.status(200).send({
+      success: true,
+      message: "Single Category get Successfully",
+      category,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      error,
+      message: "Error in getiing single Category ",
+    });
+  }
+};
+
+
+
+//Delete category
+export const deleteCategoryController = async (req, res) => {
+    try {
+        const {id}= req.params
+        await categoryModel.findByIdAndDelete(id)
+        res.status(200).send({
+            success: true,
+            message: "Delete Category Successful"
+            
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.status(500).send({
+            success: false,
+            error,
+            message: "Error in Deleting Category ",
+          });
+    }
+    
+}
---
+++ Server/routes/categoryRoute.js
@@ -0,0 +1,44 @@
+import express from "express";
+import { isAdmin, requireSignIn } from "../midllewares/authMiddleware.js";
+import {
+  categoryController,
+  createCategoryController,
+  deleteCategoryController,
+  singleCategoryController,
+  updateCategoryController,
+} from "../controllers/categoryController.js";
+
+const router = express.Router();
+
+//Routes
+//Create Category
+router.post(
+  "/create-category",
+  requireSignIn,
+  isAdmin,
+  createCategoryController
+);
+
+//Update Category
+router.put(
+  "/update-category/:id",
+  requireSignIn,
+  isAdmin,
+  updateCategoryController
+);
+
+//Get All Category
+router.get("/get-category", categoryController);
+
+//Get Single Category
+router.get("/single-category/:slug", singleCategoryController);
+
+//Delete Category
+router.delete(
+  "/delete-category/:id",
+  requireSignIn,
+  isAdmin,
+  deleteCategoryController
+);
+
+export default router;
